7d3a1453e4d7cb71180acf4f0032164a3f582a4b
Piyush Modi <modipiyush2001@gmail.com>
2023-02-12 04:57:49 +0530

    fixed pip

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/piyush/Tri-NIT-Hack/venv"
+set -gx VIRTUAL_ENV "/home/piyush/Downloads/trinit_hacks/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
