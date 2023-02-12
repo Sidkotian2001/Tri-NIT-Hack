f24cf361fc9d98c549d91fab48225517e7228ccb
Piyush-M01 <79186088+Piyush-M01@users.noreply.github.com>
2023-02-12 06:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def get_temp_rainfall(state,harvest_month):
     print(tavg,prcp)
     return tavg,prcp
 
-@app.route('/predict',method=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
 
     months = {
